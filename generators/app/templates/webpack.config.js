bb0e80144fc44272263e1d94608adf20231abcd8
Arno V <versini@gmail.com>
2018-06-15 20:23:09 -0700

    chore: migrating to webpack 4

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,104 +1,105 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const {
+  removeEmpty,
+  propIf,
+  propIfNot,
+  getIfUtils
+} = require('webpack-config-utils');
 
 const REGEX_STYLE_LOADER = /\.(css|scss)$/;
+const REGEX_BABEL_LOADER = /\.(js|jsx)$/;
+const REGEX_URL_LOADER = /\.(eot|ttf|woff|svg|png)$/;
 
-module.exports = function (env) {
-  const devtool = env === 'dev' ? 'eval-source-map' : 'source-map';
-  const rules = [
-    {
-      test: /\.(js|jsx)$/,
-      use: 'babel-loader',
-      exclude: /node_modules/
-    }
-  ];
-  const plugins = [
-    new CleanWebpackPlugin(['dist'], {
-      verbose: true,
-      dry: false
-    }),
-    new ExtractTextPlugin('build/style.[contenthash:8].css'),
-    new HtmlWebpackPlugin({
-      title:
-        env === 'dev' ?
-          'Webpack Scaffolding Developer' :
-          'Webpack Scaffolding Production',
-      template: path.join(__dirname, 'src', 'index.html')
-    })
-  ];
-
-  if (env === 'prod') {
-    plugins.push(
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        beautify: false,
-        sourceMap: true,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true
-        },
-        compress: {
-          screw_ie8: true
-        },
-        comments: false
-      })
-    );
-    rules.push({
-      test: REGEX_STYLE_LOADER,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              localIdentName: '[local]',
-              importLoaders: 1,
-              minimize: true
-            }
-          }
-        ]
-      })
-    });
-  } else {
-    rules.push({
-      test: REGEX_STYLE_LOADER,
-      use: [
-        {
-          loader: 'style-loader'
-        },
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true,
-            localIdentName: '[local]',
-            importLoaders: 1
-          }
-        }
-      ]
-    });
-  }
-
+module.exports = function(env) {
+  const { ifNotProd, ifProd } = getIfUtils(env);
   return {
-    entry: './src/index.jsx',
+    entry: {
+      app: './src/index.jsx',
+      vendor: ['react', 'react-dom']
+    },
+    mode: ifProd() ? 'production' : 'development',
     output: {
       filename: 'js/[name].bundle.[hash].js',
       path: path.resolve(__dirname, 'build')
     },
     devServer: {
       // disable security check for local development
-      disableHostCheck: (env !== 'prod')
+      disableHostCheck: ifNotProd()
     },
-    devtool,
-    plugins,
+    devtool: ifProd() ? 'eval-source-map' : 'source-map',
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'initial',
+            name: 'vendor',
+            test: 'vendor',
+            enforce: true
+          }
+        }
+      },
+      runtimeChunk: true,
+      minimizer: removeEmpty([
+        ifProd(new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true
+        })),
+        ifProd(new OptimizeCSSAssetsPlugin({}))
+      ])
+    },
+    plugins: removeEmpty([
+      new HtmlWebpackPlugin({
+        title: ifNotProd()
+          ? 'Mini Webpack Testbed [dev]'
+          : 'Mini Webpack Testbed [prod]',
+        template: path.join(__dirname, 'src', 'index.html')
+      }),
+      ifProd(new MiniCssExtractPlugin('build/style.[contenthash:8].css')),
+      ifProd(
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': JSON.stringify('production')
+        })
+      )
+    ]),
     module: {
-      rules
+      rules: removeEmpty([
+        {
+          test: REGEX_URL_LOADER,
+          use: 'url-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: REGEX_BABEL_LOADER,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        ifProd({
+          test: REGEX_STYLE_LOADER,
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        }),
+        ifNotProd({
+          test: REGEX_STYLE_LOADER,
+          use: [
+            {
+              loader: 'style-loader'
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                localIdentName: '[local]',
+                importLoaders: 1
+              }
+            }
+          ]
+        })
+      ])
     }
   };
 };
