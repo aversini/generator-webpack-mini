3119b85f8a2de45e6ff0053ed421f371403e84df
Arno V <versini@gmail.com>
2018-06-16 10:27:26 -0700

    docs: better feedback post-installation

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,8 +1,29 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
+const yosay = require('yosay');
 
 module.exports = class extends Generator {
+  prompting() {
+    this.log(
+      yosay('Welcome to the ' + chalk.blue('webpack-mini') + ' generator!')
+    );
+
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'webpack-mini',
+        message: 'About to generate code in the current folder, continue?',
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    });
+  }
+
   writing() {
     this.fs.copy(
       this.templatePath('README.md'),
@@ -14,10 +35,7 @@ module.exports = class extends Generator {
       this.destinationPath('.babelrc')
     );
 
-    this.fs.copy(
-      this.templatePath('.npmrc'),
-      this.destinationPath('.npmrc')
-    );
+    this.fs.copy(this.templatePath('.npmrc'), this.destinationPath('.npmrc'));
 
     this.fs.copy(
       this.templatePath('package.json'),
@@ -51,12 +69,17 @@ module.exports = class extends Generator {
   }
 
   end() {
-    const howToInstall = `Please run ${chalk.yellow.bold('npm install')} or ${chalk.yellow.bold('yarn')} to install dependencies.`;
+    const introduction = `Mini webpack project with React is ready!`;
+    const howToInstall = `Please run ${chalk.blue(
+      'npm install'
+    )} or ${chalk.blue('yarn')} to install dependencies.`;
+    const postInstall = `Check the ${chalk.blue(
+      'README.md'
+    )} file for more information (build procedures, etc.)`;
+
     this.log();
-    this.log('Mini webpack project with React is ready!');
+    this.log(yosay(introduction));
     this.log(howToInstall);
-    this.log(
-      'And check the README.md file for more information (build procedures, etc.)'
-    );
+    this.log(postInstall);
   }
 };
