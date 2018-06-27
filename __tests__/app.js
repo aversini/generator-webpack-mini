f8f6e40f9bcd92b92c1bb437e794fed9b7533f02
Arno V <versini@gmail.com>
2018-06-26 18:52:53 -0700

    feat: adding support for both wp3 and wp4

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,24 +4,72 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-webpack-mini:app (React)', () => {
-  beforeAll(() => {
+  it('does not generate a project', () => {
+    // The object returned acts like a promise, so
+    // return it to wait until the process is done
     return helpers
-      .run(path.join(__dirname, '../generators/app'));
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({continue: false}) // Mock the prompt answers
+      .then(function () {
+        assert.noFile([
+          '.babelrc',
+          '.npmrc',
+          '.gitignore',
+          'README.md',
+          'package.json',
+          'webpack.config.js',
+          'src/favicon.ico',
+          'src/index.html',
+          'src/testbed.css',
+          'src/testbed.jsx',
+          'src/index.jsx'
+        ]);
+      });
   });
-
-  it('creates files', () => {
-    assert.file([
-      '.babelrc',
-      '.npmrc',
-      '.gitignore',
-      'README.md',
-      'package.json',
-      'webpack.config.js',
-      'src/favicon.ico',
-      'src/index.html',
-      'src/testbed.css',
-      'src/testbed.jsx',
-      'src/index.jsx'
-    ]);
+  it('generate a wp3 project', () => {
+    // The object returned acts like a promise, so
+    // return it to wait until the process is done
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({continue: true}) // Mock the prompt answers
+      .withPrompts({wp4: false}) // Mock the prompt answers
+      .then(function () {
+        assert.file([
+          '.babelrc',
+          '.npmrc',
+          '.gitignore',
+          'README.md',
+          'package.json',
+          'webpack.config.js',
+          'src/favicon.ico',
+          'src/index.html',
+          'src/testbed.css',
+          'src/testbed.jsx',
+          'src/index.jsx'
+        ]);
+      });
+  });
+  it('generate a wp4 project', () => {
+    // The object returned acts like a promise, so
+    // return it to wait until the process is done
+    return helpers
+      .run(path.join(__dirname, '../generators/app'))
+      .withPrompts({continue: true}) // Mock the prompt answers
+      .withPrompts({wp4: true}) // Mock the prompt answers
+      .then(function () {
+        assert.file([
+          '.babelrc',
+          '.npmrc',
+          '.gitignore',
+          'README.md',
+          'package.json',
+          'webpack.config.js',
+          'src/favicon.ico',
+          'src/index.html',
+          'src/testbed.css',
+          'src/testbed.jsx',
+          'src/index.jsx'
+        ]);
+      });
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -12,7 +12,13 @@ module.exports = class extends Generator {
     const prompts = [
       {
         type: 'confirm',
-        name: 'webpack-mini',
+        name: 'wp4',
+        message: 'Targeting Webpack v4+?',
+        default: true
+      },
+      {
+        type: 'confirm',
+        name: 'continue',
         message: 'About to generate code in the current folder, continue?',
         default: true
       }
@@ -25,74 +31,90 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('README.md'),
-      this.destinationPath('README.md')
-    );
+    if (this.props.continue) {
+      this.fs.copy(
+        this.templatePath('README.md'),
+        this.destinationPath('README.md')
+      );
 
-    this.fs.copy(
-      this.templatePath('.babelrc'),
-      this.destinationPath('.babelrc')
-    );
+      this.fs.copy(
+        this.templatePath('.babelrc'),
+        this.destinationPath('.babelrc')
+      );
 
-    this.fs.copy(
-      this.templatePath('.npmrc'),
-      this.destinationPath('.npmrc')
-    );
+      this.fs.copy(
+        this.templatePath('.npmrc'),
+        this.destinationPath('.npmrc')
+      );
 
-    this.fs.copy(
-      this.templatePath('.gitignore'),
-      this.destinationPath('.gitignore')
-    );
+      this.fs.copy(
+        this.templatePath('.gitignore'),
+        this.destinationPath('.gitignore')
+      );
 
-    this.fs.copy(
-      this.templatePath('package.json'),
-      this.destinationPath('package.json')
-    );
+      if (this.props.wp4) {
+        this.fs.copy(
+          this.templatePath('wp4.webpack.config.js'),
+          this.destinationPath('webpack.config.js')
+        );
+        this.fs.copy(
+          this.templatePath('wp4.package.json'),
+          this.destinationPath('package.json')
+        );
+      } else {
+        this.fs.copy(
+          this.templatePath('wp3.webpack.config.js'),
+          this.destinationPath('webpack.config.js')
+        );
+        this.fs.copy(
+          this.templatePath('wp3.package.json'),
+          this.destinationPath('package.json')
+        );
+      }
 
-    this.fs.copy(
-      this.templatePath('webpack.config.js'),
-      this.destinationPath('webpack.config.js')
-    );
+      this.fs.copy(
+        this.templatePath('src', 'index.html'),
+        this.destinationPath('src', 'index.html')
+      );
 
-    this.fs.copy(
-      this.templatePath('src', 'index.html'),
-      this.destinationPath('src', 'index.html')
-    );
+      this.fs.copy(
+        this.templatePath('src', 'testbed.css'),
+        this.destinationPath('src', 'testbed.css')
+      );
 
-    this.fs.copy(
-      this.templatePath('src', 'testbed.css'),
-      this.destinationPath('src', 'testbed.css')
-    );
+      this.fs.copy(
+        this.templatePath('src', 'index.jsx'),
+        this.destinationPath('src', 'index.jsx')
+      );
 
-    this.fs.copy(
-      this.templatePath('src', 'index.jsx'),
-      this.destinationPath('src', 'index.jsx')
-    );
+      this.fs.copy(
+        this.templatePath('src', 'testbed.jsx'),
+        this.destinationPath('src', 'testbed.jsx')
+      );
 
-    this.fs.copy(
-      this.templatePath('src', 'testbed.jsx'),
-      this.destinationPath('src', 'testbed.jsx')
-    );
-
-    this.fs.copy(
-      this.templatePath('src', 'favicon.ico'),
-      this.destinationPath('src', 'favicon.ico')
-    );
+      this.fs.copy(
+        this.templatePath('src', 'favicon.ico'),
+        this.destinationPath('src', 'favicon.ico')
+      );
+    }
   }
 
   end() {
-    const introduction = `Mini webpack project with React is ready!`;
-    const howToInstall = `Please run ${chalk.blue(
-      'npm install'
-    )} or ${chalk.blue('yarn')} to install dependencies.`;
-    const postInstall = `Check the ${chalk.blue(
-      'README.md'
-    )} file for more information (build procedures, etc.)`;
+    if (this.props.continue) {
+      const introduction = `Mini webpack project with React is ready!`;
+      const howToInstall = `Please run ${chalk.blue(
+        'npm install'
+      )} or ${chalk.blue('yarn')} to install dependencies.`;
+      const postInstall = `Check the ${chalk.blue(
+        'README.md'
+      )} file for more information (build procedures, etc.)`;
 
-    this.log();
-    this.log(yosay(introduction));
-    this.log(howToInstall);
-    this.log(postInstall);
+      this.log();
+      this.log(yosay(introduction));
+      this.log(howToInstall);
+      this.log(postInstall);
+    } else {
+      this.log('Bye then...');
+    }
   }
 };
